e560ca73297ae4f253c2687eaaf283f211e2cca1
Seda Saglik <sdasaglik@gmail.com>
2026-07-27 17:29:37 +0300

    fix: iOS uygulamasinda kayit sayfasi engellendi (Apple 3.1.1)

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,18 @@ Route::get('/', function () { return view('welcome'); });
 Route::get('/gizlilik', function () { return view('gizlilik'); })->name('gizlilik');
 Route::get('/kullanim-sartlari', function () { return view('kullanim-sartlari'); })->name('kullanim-sartlari');
 Route::get('/destek', function () { return view('destek'); })->name('public.destek');
-Route::get('/kayit', [RegisterController::class, 'create'])->name('register.form');
-Route::post('/kayit', [RegisterController::class, 'store'])->name('register.store');
+Route::get('/kayit', function(\Illuminate\Http\Request $request) {
+    if ($request->userAgent() && str_contains($request->userAgent(), 'LattessaApp')) {
+        return redirect('/giris');
+    }
+    return app()->call([\App\Http\Controllers\Auth\RegisterController::class, 'create']);
+})->name('register.form');
+Route::post('/kayit', function(\Illuminate\Http\Request $request) {
+    if ($request->userAgent() && str_contains($request->userAgent(), 'LattessaApp')) {
+        return redirect('/giris');
+    }
+    return app()->call([\App\Http\Controllers\Auth\RegisterController::class, 'store']);
+})->name('register.store');
 
 // E-posta ile hesap bulma ve giris yonlendirme
 Route::get('/giris', function () {
