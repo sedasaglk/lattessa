8147285aa9796b2261301e48ae7e71c274c3b9fc
Seda Saglik <sdasaglik@gmail.com>
2026-07-29 11:49:57 +0300

    Register Firebase service provider manually

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\EventServiceProvider;
+use Kreait\Laravel\Firebase\ServiceProvider as FirebaseServiceProvider;
 
 return [
     AppServiceProvider::class,
     EventServiceProvider::class,
+    FirebaseServiceProvider::class,
 ];
