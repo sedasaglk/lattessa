4161fd9e3cc6b6b576369452510e0c336b8c1a1e
Seda Saglik <sdasaglik@gmail.com>
2026-07-29 14:20:17 +0300

    fix: panel.dashboard route eklendi

--- routes/web.php
+++ routes/web.php
@@ -123,6 +123,7 @@ Route::prefix('{tenant_slug}')->middleware('tenant')->group(function () {
         Route::delete('/fcm-token', [FcmTokenController::class, 'destroy'])->name('panel.fcm.destroy');
 
         Route::get('/', fn($tenant_slug) => redirect()->route('panel.appointments.index', ['tenant_slug' => $tenant_slug]))->name('tenant.home');
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('panel.dashboard');
 
         Route::get('/randevular', [AppointmentController::class, 'index'])->name('panel.appointments.index');
         Route::get('/randevular/events', [AppointmentController::class, 'calendarEvents'])->name('panel.appointments.events');
